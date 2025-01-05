2797a078f196549528f8c78d86c79f01063e8aa2
Ali <ali7.khosrojerdi@gmail.com>
2025-01-05 20:51:03 +0330

    add: plan controller

---
+++ PlanningAppMvc/Controllers/PlanController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanningAppMvc.Models;
+
+namespace PlanningAppMvc.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly PlannigContext _context;
+        public PlanController(PlannigContext context)
+        {
+            _context = context;
+        }
+    }
+}
--- PlanningAppMvc/obj/Debug/net8.0/PlanningAppMvc.AssemblyInfo.cs
+++ PlanningAppMvc/obj/Debug/net8.0/PlanningAppMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanningAppMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7e3a2cd32f0cae8abc920b3afbc725053b6150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a853611c633c12ddbb0585481ee1e06a1a6343e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanningAppMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlanningAppMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
