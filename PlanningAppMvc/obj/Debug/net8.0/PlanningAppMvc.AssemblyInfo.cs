cb7b9db8e8e526c7971ca665067b049114c68d74
Ali <ali7.khosrojerdi@gmail.com>
2025-01-10 17:11:17 +0330

    commit to push

--- PlanningAppMvc/obj/Debug/net8.0/PlanningAppMvc.AssemblyInfo.cs
+++ PlanningAppMvc/obj/Debug/net8.0/PlanningAppMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanningAppMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0982b4573f2ae40e532e6abbf8f767fcc0dbea7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b2b6711efa4be5e84baff42ee8f0d576d68b58")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanningAppMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlanningAppMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
